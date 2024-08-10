0f0cb43db1d2ea220d3e5022d1b81f561b8291ac
mozart <mozart@localhost.com>
2024-08-10 06:23:19 -0400

    ts

--- axios.ts
+++ axios.ts
@@ -1,16 +1,16 @@
-async function get (url) {
-    const MAX_WAIT = 10;//seconds
+async function get(url: string): Promise<Response | string> {
+    const MAX_WAIT = 10; // seconds
     const st = new Date();
     const controller = new AbortController();
     const request = new Request(url, {
         signal: controller.signal,
     });
-    const timeout = new Promise(res => {
-            setTimeout(() => {
-                    res(`Request took more than ${MAX_WAIT } seconds`);
-                    controller.abort();
-                    }, MAX_WAIT * 1000);
-    })
+    const timeout: Promise<string> = new Promise(res => {
+        setTimeout(() => {
+            res(`Request took more than ${MAX_WAIT} seconds`);
+            controller.abort();
+        }, MAX_WAIT * 1000);
+    });
 
     return Promise.race([fetch(request), timeout]);
 }
@@ -18,8 +18,8 @@ async function get (url) {
 (async () => {
     const res = await get('http://localhost:3000/');
     try {
-        console.log(await res.text());
+        console.log(await (res as Response).text());
     } catch {
         console.log(res);
     }
-})()
+})();
